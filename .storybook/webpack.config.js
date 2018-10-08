32dc1378d77a1af35227c50f066655c0abaa5e57
moddaman <ovrebo90@gmail.com>
2018-10-08 14:12:20 +0200

    Added generated static files to .gitignore

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,25 +1,13 @@
 const path = require('path');
 const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
+
 module.exports = (baseConfig, env, defaultConfig) => {
+
   defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('ts-loader'),
   });
-  defaultConfig.module.rules.push({
-    test: /\.scss$/,
-    loaders: [
-      'style-loader',
-      {
-        loader: require.resolve('css-loader'),
-        options: {
-          importLoaders: 1,
-          modules: true,
-          localIdentName: '[name]__[local]__[hash:base64:5]',
-        },
-      },
-    ],
-    include: path.resolve(__dirname, '../'),
-  });
+ 
   defaultConfig.plugins.push(new TSDocgenPlugin());
   defaultConfig.resolve.extensions.push('.ts', '.tsx');
   return defaultConfig;
