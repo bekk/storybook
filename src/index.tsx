657b81c5574669d94d6930e6e265639445f8f3e9
moddaman <ovrebo90@gmail.com>
2018-10-18 10:35:56 +0200

    Exports NotificationTypes. This should make it easier to configure Notification

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 export { Notification } from './components/Notification/Notification';
+export { NotificationTypes } from './components/Notification/NotificationTypes';
 export { TextField } from './components/TextField/TextField';
 export { Icon } from './basic/Icon/Icon';
 export { Color } from './basic/Color/Color';
