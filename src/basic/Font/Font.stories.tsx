1ec85739eae01271d017eb28cbd973500fc80376
Henrik Limseth <henrik.limseth@bekk.no>
2018-11-07 16:57:38 +0100

    Add new web fonts

--- src/basic/Font/Font.stories.tsx
+++ src/basic/Font/Font.stories.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { wInfo } from "../../utils";
-import './Font.css'
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { wInfo } from '../../utils';
+import './Font.css';
 
-function createDivStyle(color:string):any {
+function createDivStyle(color: string): any {
   return {
     height: '100px',
     width: '100px',
@@ -11,28 +11,36 @@ function createDivStyle(color:string):any {
   };
 }
 
-(storiesOf("Basic/Font", module) as any)
+(storiesOf('Basic/Font', module) as any)
   .addWithJSX(
-    "Om fontene",
+    'Om fontene',
     wInfo(`
         Viktig! Fontene er lisensiert :( . Sjekk med Forvaltning før du tar dem i bruk.
 
-  `)
-    (() => (
-      <div style={createDivStyle('var(--svart)')}></div>
-    ))
-  ).addWithJSX(
-    "FFDINWebProLight",
-    (() => (
-      <div className="FFDINWebProLight">
-          Gresshoppen sprang utover mens han sang "Quackquack pata laxa Ææ Øø Åå Zz"
-      </div>
-    ))
-  ).addWithJSX(
-    "NewZaldBook",
-    (() => (
-      <div className="NewZaldBook">
-          Gresshoppen sprang utover mens han sang "Quackquack pata laxa Ææ Øø Åå Zz"
-      </div>
-    ))
-  );
+  `)(() => <div style={createDivStyle('var(--svart)')} />)
+  )
+  .addWithJSX('FFDINWebProLight', () => (
+    <div className="FFDINWebProLight">
+      Gresshoppen sprang utover mens han sang "Quackquack pata laxa Ææ Øø Åå Zz"
+    </div>
+  ))
+  .addWithJSX('NewZaldBook', () => (
+    <div className="NewZaldBook">
+      Gresshoppen sprang utover mens han sang "Quackquack pata laxa Ææ Øø Åå Zz"
+    </div>
+  ))
+  .addWithJSX('DINW01LightItalic', () => (
+    <div className="DINW01LightItalic">
+      Gresshoppen sprang utover mens han sang "Quackquack pata laxa Ææ Øø Åå Zz"
+    </div>
+  ))
+  .addWithJSX('DINW01Regular', () => (
+    <div className="DINW01Regular">
+      Gresshoppen sprang utover mens han sang "Quackquack pata laxa Ææ Øø Åå Zz"
+    </div>
+  ))
+  .addWithJSX('DINW01Medium', () => (
+    <div className="DINW01Medium">
+      Gresshoppen sprang utover mens han sang "Quackquack pata laxa Ææ Øø Åå Zz"
+    </div>
+  ));
