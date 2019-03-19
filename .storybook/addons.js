610de9c76783008c7f1cd832a3bb5e5b805b1a2c
Henrik Limseth <henrik.limseth@bekk.no>
2019-03-19 12:56:29 +0100

    Remove unused addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,4 @@
-import '@storybook/addon-actions/register';
+import 'storybook-addon-jsx/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-backgrounds/register';
-import 'storybook-addon-jsx/register';
