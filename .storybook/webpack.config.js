50978f2e44379b6e94833c4cbe0460d52a9667ee
Henrik Limseth <henrik.limseth@bekk.no>
2019-03-25 11:13:50 +0100

    Add story source

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -13,8 +13,13 @@ module.exports = ({ config }) => {
           configFileName: './.storybook/tsconfig.json'
         }
       },
-      { loader: require.resolve('react-docgen-typescript-loader') }
-    ]
+      { loader: require.resolve('react-docgen-typescript-loader') },
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' }
+      }
+    ],
+    enforce: 'pre'
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
